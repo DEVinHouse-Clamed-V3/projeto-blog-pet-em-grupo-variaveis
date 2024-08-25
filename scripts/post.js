aeb4584a2074161e25fea9cd23fe86d61c94b7b7
Calegari1712 <silvavitor492@gmail.com>
2024-08-25 16:26:28 -0300

    JAVA E HTML POST

---
+++ scripts/post.js
@@ -0,0 +1,21 @@
+function carregarPosts () {
+
+    const postsNoLocalStorage = JSON.parse(localStorage.getItem("posts"))
+    
+//PEGAR O ID NA URL
+    const url  = new URL (window.location.href)
+    const params = new URLSearchParams(url.search)
+    const idUrl = params.get('id')
+//PROCURANDO O ID DA URL NO LOCAL STORAGE
+    const postEncontrado = postsNoLocalStorage.find((item) => item.id == idUrl)
+
+    document.getElementById('nome').innerText = postEncontrado.titulo
+    document.getElementById('foto').setAttribute('src', postEncontrado.foto)
+
+    document.getElementById('descricao').innerText = postEncontrado.descricao
+    document.getElementById('categoria').innerText = postEncontrado.categoria
+    document.getElementById('data').innerText = postEncontrado.dataCriacao
+}
+
+
+document.addEventListener('DOMContentLoaded', carregarPosts)
